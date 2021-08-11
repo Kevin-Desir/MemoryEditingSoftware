eb26b64dd634ee2b6545be735479c1fbe2371901
Kevin Desir <desirkevin@outlook.com>
2021-08-11 18:54:27 +0200

    Update Run Module

--- MemoryEditingSoftware.Run/Views/ReadItem.xaml.cs
+++ MemoryEditingSoftware.Run/Views/ReadItem.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using MemoryEditingSoftware.Core.Entities;
+using System;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Controls;
 
 namespace MemoryEditingSoftware.Run.Views
@@ -9,12 +11,47 @@ namespace MemoryEditingSoftware.Run.Views
     /// </summary>
     public partial class ReadItem : UserControl
     {
+        [DllImport(@"MemoryManipulation.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double ReadDoubleFromMemory(string address);
+
+        [DllImport(@"MemoryManipulation.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int ReadIntFromMemory(string address);
+
         public ReadItem(EditItem editItem)
         {
             InitializeComponent();
 
             name.Text = editItem.Name;
-            val.Text = editItem.Value;
+            val.Text = "No value received / Wrong format, please check address via editor";
+
+            try
+            {
+                if (val.Text.Contains("."))
+                {
+                    ThreadPool.QueueUserWorkItem(_ => StartReadingDouble(editItem.Address));
+                }
+                else
+                {
+                    ThreadPool.QueueUserWorkItem(_ => StartReadingInt(editItem.Address));
+                }
+            }
+            catch (Exception) { }
+
+
+
         }
+
+        // 2 separate methods for double or int to make the parsing only once instead of 
+        // continually in the threads
+        private void StartReadingDouble(string address)
+        {
+            val.Text = ReadDoubleFromMemory(address).ToString();
+        }
+
+        private void StartReadingInt(string address)
+        {
+            val.Text = ReadIntFromMemory(address).ToString();
+        }
+        
     }
 }
