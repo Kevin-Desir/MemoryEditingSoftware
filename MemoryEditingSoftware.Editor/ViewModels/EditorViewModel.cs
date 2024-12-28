871244755ec1bfceaf94b24073465a9f356298db
Kevin-Desir <desirkevin@outlook.com>
2024-12-28 03:01:26 +0100

    Drag Drop editor grid

--- MemoryEditingSoftware.Editor/ViewModels/EditorViewModel.cs
+++ MemoryEditingSoftware.Editor/ViewModels/EditorViewModel.cs
@@ -21,9 +21,11 @@ namespace MemoryEditingSoftware.Editor.ViewModels
         private ContentControl editorGridContentControl;
         public ContentControl EditorGridContentControl
         {
-            get {  return editorGridContentControl; } 
-            set { SetProperty(ref editorGridContentControl, value); } 
+            get { return editorGridContentControl; }
+            set { SetProperty(ref editorGridContentControl, value); }
         }
+
+        private Grid grid;
 
         #endregion
 
@@ -35,11 +37,16 @@ namespace MemoryEditingSoftware.Editor.ViewModels
 
         public EditorViewModel()
         {
-            Grid grid = new Grid();
+            grid = new Grid();
+            grid.ShowGridLines = true;
+
             grid.Background = Brushes.Magenta;
             grid.ColumnDefinitions.Add(new ColumnDefinition());
             grid.ColumnDefinitions.Add(new ColumnDefinition());
             grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
             grid.RowDefinitions.Add(new RowDefinition());
             grid.RowDefinitions.Add(new RowDefinition());
             grid.RowDefinitions.Add(new RowDefinition());
@@ -54,28 +61,111 @@ namespace MemoryEditingSoftware.Editor.ViewModels
 
             grid.Children.Add(componentView);
 
-            ComponentView componentView2 = new ComponentView(new EditItem("Address 5", "name 5", "value 5", true));
-            Grid.SetRow(componentView2, 2);
-            Grid.SetRowSpan(componentView2, 1);
+            //ComponentView componentView2 = new ComponentView(new EditItem("Address 5", "name 5", "value 5", true));
+            //Grid.SetRow(componentView2, 2);
+            //Grid.SetRowSpan(componentView2, 1);
 
-            Grid.SetColumn(componentView2, 2);
-            Grid.SetColumnSpan(componentView2, 1);
+            //Grid.SetColumn(componentView2, 2);
+            //Grid.SetColumnSpan(componentView2, 1);
 
-            grid.Children.Add(componentView2);
+            //grid.Children.Add(componentView2);
 
             EditorGridContentControl = new ContentControl();
             EditorGridContentControl.Content = grid;
+
+            grid.AllowDrop = true;
+            grid.Drop += mainGrid_Drop;
+            grid.DragOver += mainGrid_DragOver;
+
         }
 
         #endregion
 
         #region Private Methods
 
-        #endregion
+        // Handle the DragOver event to allow dropping
+        private void mainGrid_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Move;
+            e.Handled = true;
+        }
 
-        #region Public Methods
+        // Handle the drop event
+        private void mainGrid_Drop(object sender, DragEventArgs e)
+        {
+            Canvas draggedElement = e.Data.GetData(typeof(Canvas)) as Canvas;
 
-        #endregion
+            if (draggedElement == null)
+                return;
 
+            ComponentView componentView = ((draggedElement.Parent as Grid).Parent as Border).Parent as ComponentView;
+
+
+            // Get the mouse position and calculate the target grid cell
+            var position = e.GetPosition(grid);
+
+            int targetRow = GetRowFromPosition(position.Y);
+            int targetColumn = GetColumnFromPosition(position.X);
+
+            if (targetRow >= 0 && targetColumn >= 0)
+            {
+                switch (draggedElement.Name)
+                {
+                    case "top":
+                        componentView.SetValue(Grid.RowSpanProperty, (int)componentView.GetValue(Grid.RowProperty) + (int)componentView.GetValue(Grid.RowSpanProperty) - targetRow);
+                        componentView.SetValue(Grid.RowProperty, targetRow);
+                        break;
+                    case "bottom":
+                        componentView.SetValue(Grid.RowSpanProperty, targetRow - (int)componentView.GetValue(Grid.RowProperty) + 1);
+                        break;
+                    case "left":
+                        componentView.SetValue(Grid.ColumnSpanProperty, (int)componentView.GetValue(Grid.ColumnProperty) + (int)componentView.GetValue(Grid.ColumnSpanProperty) - targetColumn);
+                        componentView.SetValue(Grid.ColumnProperty, targetColumn);
+                        break;
+                    case "right":
+                        componentView.SetValue(Grid.ColumnSpanProperty, targetColumn - (int)componentView.GetValue(Grid.ColumnProperty) + 1);
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Row = {componentView.GetValue(Grid.RowProperty)}\tRowSpan = {componentView.GetValue(Grid.RowSpanProperty)}\n" +
+                $"Column = {componentView.GetValue(Grid.ColumnProperty)} + ColumnSpan = {componentView.GetValue(Grid.ColumnSpanProperty)}");
+
+            draggedElement = null; // Clear the reference
+        }
+
+        // Get the row index based on the Y-coordinate
+        private int GetRowFromPosition(double y)
+        {
+            double cumulativeHeight = 0;
+            for (int i = 0; i < grid.RowDefinitions.Count; i++)
+            {
+                cumulativeHeight += grid.RowDefinitions[i].ActualHeight;
+                if (y < cumulativeHeight)
+                    return i;
+            }
+            return -1; // Not found
+        }
+
+        // Get the column index based on the X-coordinate
+        private int GetColumnFromPosition(double x)
+        {
+            double cumulativeWidth = 0;
+            for (int i = 0; i < grid.ColumnDefinitions.Count; i++)
+            {
+                cumulativeWidth += grid.ColumnDefinitions[i].ActualWidth;
+                if (x < cumulativeWidth)
+                    return i;
+            }
+            return -1; // Not found
+        }
     }
+
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+
 }
+
--- MemoryEditingSoftware.Editor/Views/ComponentView.xaml.cs
+++ MemoryEditingSoftware.Editor/Views/ComponentView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MemoryEditingSoftware.Core.Entities;
 using Prism.Commands;
 using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -12,6 +13,8 @@ namespace MemoryEditingSoftware.Editor.Views
     public partial class ComponentView : UserControl
     {
         #region Properties
+
+        private UIElement _draggedElement; // Store the element being dragged
 
         #endregion
 
@@ -38,62 +41,72 @@ namespace MemoryEditingSoftware.Editor.Views
 
         #endregion
 
-        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        //private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        //{
+        //    if (sender is Button button)
+        //    {
+        //        switch (button.Name)
+        //        {
+        //            case "top":
+        //                this.SetValue(Grid.RowProperty, (int) this.GetValue(Grid.RowProperty) - 1);
+        //                this.SetValue(Grid.RowSpanProperty, (int)this.GetValue(Grid.RowSpanProperty) + 1);
+        //                break;
+        //            case "bottom":
+        //                this.SetValue(Grid.RowSpanProperty, (int) this.GetValue(Grid.RowSpanProperty) + 1);
+        //                break;
+        //            case "left":
+        //                this.SetValue(Grid.ColumnProperty, (int) this.GetValue(Grid.ColumnProperty) - 1);
+        //                this.SetValue(Grid.ColumnSpanProperty, (int) this.GetValue(Grid.ColumnSpanProperty) + 1);
+        //                break;
+        //            case "right":
+        //                this.SetValue(Grid.ColumnSpanProperty, (int) this.GetValue(Grid.ColumnSpanProperty) + 1);
+        //                break;
+        //        }
+        //    }
+        //}
+
+        //private void Button_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    if (sender is Button button)
+        //    {
+        //        switch (button.Name)
+        //        {
+        //            case "top":
+        //                this.SetValue(Grid.RowProperty, (int)this.GetValue(Grid.RowProperty) + 1);
+        //                this.SetValue(Grid.RowSpanProperty, (int)this.GetValue(Grid.RowSpanProperty) - 1);
+        //                break;
+        //            case "bottom":
+        //                this.SetValue(Grid.RowSpanProperty, (int)this.GetValue(Grid.RowSpanProperty) - 1);
+        //                break;
+        //            case "left":
+        //                this.SetValue(Grid.ColumnProperty, (int)this.GetValue(Grid.ColumnProperty) + 1);
+        //                this.SetValue(Grid.ColumnSpanProperty, (int)this.GetValue(Grid.ColumnSpanProperty) - 1);
+        //                break;
+        //            case "right":
+        //                this.SetValue(Grid.ColumnSpanProperty, (int)this.GetValue(Grid.ColumnSpanProperty) - 1);
+        //                break;
+        //        }
+        //    }
+        //}
+
+        //private void top_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    this.Cursor = Cursors.ScrollN;
+        //}
+
+        //private void top_MouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    this.Cursor = null;
+        //}
+
+        // Handle the start of the drag operation
+        private void Canvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (sender is Button button)
+            if (sender is Canvas element)
             {
-                switch (button.Name)
-                {
-                    case "top":
-                        this.SetValue(Grid.RowProperty, (int) this.GetValue(Grid.RowProperty) - 1);
-                        this.SetValue(Grid.RowSpanProperty, (int)this.GetValue(Grid.RowSpanProperty) + 1);
-                        break;
-                    case "bottom":
-                        this.SetValue(Grid.RowSpanProperty, (int) this.GetValue(Grid.RowSpanProperty) + 1);
-                        break;
-                    case "left":
-                        this.SetValue(Grid.ColumnProperty, (int) this.GetValue(Grid.ColumnProperty) - 1);
-                        this.SetValue(Grid.ColumnSpanProperty, (int) this.GetValue(Grid.ColumnSpanProperty) + 1);
-                        break;
-                    case "right":
-                        this.SetValue(Grid.ColumnSpanProperty, (int) this.GetValue(Grid.ColumnSpanProperty) + 1);
-                        break;
-                }
+                _draggedElement = element; // Keep a reference to the dragged element
+                DragDrop.DoDragDrop(element, element, DragDropEffects.Move);
             }
-        }
-
-        private void Button_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            if (sender is Button button)
-            {
-                switch (button.Name)
-                {
-                    case "top":
-                        this.SetValue(Grid.RowProperty, (int)this.GetValue(Grid.RowProperty) + 1);
-                        this.SetValue(Grid.RowSpanProperty, (int)this.GetValue(Grid.RowSpanProperty) - 1);
-                        break;
-                    case "bottom":
-                        this.SetValue(Grid.RowSpanProperty, (int)this.GetValue(Grid.RowSpanProperty) - 1);
-                        break;
-                    case "left":
-                        this.SetValue(Grid.ColumnProperty, (int)this.GetValue(Grid.ColumnProperty) + 1);
-                        this.SetValue(Grid.ColumnSpanProperty, (int)this.GetValue(Grid.ColumnSpanProperty) - 1);
-                        break;
-                    case "right":
-                        this.SetValue(Grid.ColumnSpanProperty, (int)this.GetValue(Grid.ColumnSpanProperty) - 1);
-                        break;
-                }
-            }
-        }
-
-        private void top_MouseEnter(object sender, MouseEventArgs e)
-        {
-            this.Cursor = Cursors.ScrollN;
-        }
-
-        private void top_MouseLeave(object sender, MouseEventArgs e)
-        {
-            this.Cursor = null;
         }
     }
 }
